3d4abaa22a091e395ec0743f0ea5d02d4cd653e9
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2016-09-17 16:59:58 +0530

    added monte-carlo simulation without the win check condition

--- AI.cpp
+++ AI.cpp
@@ -1,7 +1,70 @@
+#include <algorithm>   //random shuffle
 #include "AI.h"
 
 /* AI is implemented using Monte Carlo simulation */
 
+/* finds the optimum next move for the AI. It returns the x and 
+ * y value of the move in the parameter (passed by reference) */
+void AI::calculate_next_move(unsigned short &x, unsigned short &y,
+                     unsigned short prev_x, unsigned short prev_y)
+{
+    double win_chance, max_chance = 0.0;
+    int best_move=0;
+    //refresh_simulation_board();
+    update_simulation_board(get_base_index(prev_x, prev_y),
+                                board_ref.player_symbol);
+    for(int i=0; i<size; i++){   //place ai move in each empty hex
+        if(sb[i]!='.') continue;
+        win_chance = montecarlo_simulate(i, 1000);
+        if(win_chance>max_chance){
+            max_chance = win_chance;
+            best_move = i;
+        }
+    }
+    get_x_y(best_move, x, y);
+    update_simulation_board(get_base_index(x, y),
+                                        board_ref.ai_symbol);
+    print_sim_board();
+}
+
+double AI::montecarlo_simulate(int pos, int count)
+{
+    std::vector<char> b = sb;           //copy of simulation board
+    b.at(pos) = board_ref.ai_symbol;    //place ai_move at pos
+
+    std::vector<int> simulation_moves;  //all the simulation moves (even-pl, odd-AI)
+    //populate the simulation_moves vector with all empty positions
+    for(int j=0; j<size; j++){
+        if(b[j]=='.'){ //every empty hex
+            simulation_moves.push_back(j);
+        }
+    }
+
+    double chance = 0;                  //stores the probability of win
+    char move_sym = board_ref.player_symbol;//next move symbol
+    int win_count=0;                    //number of wins
+
+    for(int i=0; i<count; i++){         //simulation count
+        //randomly shuffle the simulation vector
+        std::random_shuffle(simulation_moves.begin(), simulation_moves.end());
+        for(auto const& pos: simulation_moves){  //fill the entire board
+            b[pos] = move_sym;                   //make simulation move
+            move_sym = (move_sym=='X'? 'O': 'X');//swap move symbol for next move
+        }
+        if(ai_won(b)) win_count++;                //check if ai won
+    }
+    chance = (double)win_count/count;
+    std::cout << "DEBUG: chance = "<< chance << std::endl;
+    return chance;
+}
+
+bool AI::ai_won(std::vector<char> board)
+{
+    //implement the union-find algo on board : TODO
+    return true;
+}
+
+/*
 void AI::refresh_simulation_board() //cant we just capture the prev move? TODO
 {
     //std::fill(sb.begin(), sb.end(), '.');      //empty the board
@@ -11,56 +74,4 @@ void AI::refresh_simulation_board() //cant we just capture the prev move? TODO
         }
     }
 }
-
-/* finds the optimum next move for the AI. It returns the x and 
- * y value of the move in the parameter (passed by reference) */
-int AI::calculate_next_move(unsigned short &x, unsigned short &y)
-{
-    double win_chance, max_chance = 0.0;
-    int best_move=0;
-    refresh_simulation_board();                  //copies the hexboard into the simulation board
-    for(int i=0; i<size && sb[i]=='.'; i++){   //place ai move in each empty hex
-        win_chance = montecarlo_simulate(i, 1000);
-        if(win_chance>max_chance){
-            max_chance = win_chance;
-            best_move = i;
-        }
-    }
-    return best_move;
-}
-
-double montecarlo_simulate(int pos, int count)
-{
-    std::vector<char> b = sb;
-    b.at(pos) = board_ref.ai_symbol;
-    double chance = 0;
-    char move = board_ref.player_symbol;
-    std::vector<int> simulation_moves;
-    //insert X and O alternately in all empty positions
-    //populate the simulation_moves vector
-    for(int j=0; j<size && b[j]=='.'; j++){ //every empty hex
-        move = (move=='X'? 'O': 'X');  //swap move
-        b[j]=move;
-        simulation_moves.push_back(j);
-    }
-    for(int i=0; i<count; i++){    //simulation count
-        std::random_shuffle(simulation_moves.begin(); simulation_moves.end());
-        move = board_ref.player_symbol;        //player moves
-        for(auto const& p: simulation_moves){
-            b[p] = move;
-            move = (move=='X'? 'O': 'X');  //swap move
-        }
-        bool ai_won=false, player_won=false;
-        who_won(b, ai_won, player_won);
-        if(ai_won) ai_win_count++;
-        else player_win_count++;
-    }
-    chance = ai_win_count/player_win_count;
-    return chance;
-}
-
-void who_won(std::vector<char> board, bool &ai_won, bool &player_won)
-{
-    //implement the union-find algo on board
-    ai_won=true;
-}
+*/
--- AI.h
+++ AI.h
@@ -3,6 +3,7 @@
 
 #include "HexBoard.h"
 #include <vector>
+#include <unordered_set>
 
 /* AI(artificial intelligence) class represents the computer player
  * of the game. The only public method exposed is calculate_next_move.
@@ -13,10 +14,11 @@ class AI {
     AI(HexBoard& hb)                            //constructor
     : board_ref(hb),
       size(hb.board_size*hb.board_size) {
-          sb = vector<char>(size, '.');         //empty board
+          sb = std::vector<char>(size, '.');         //empty board
     }
 
-    bool calculate_next_move(unsigned short &x, unsigned short &y);
+    void calculate_next_move(unsigned short &x, unsigned short &y,
+                     unsigned short prev_x, unsigned short prev_y);
 
   private:
     HexBoard &board_ref;                        //reference to the actual board
@@ -25,14 +27,34 @@ class AI {
     std::unordered_set<int> vertical_boundary;  //contains top & bottom row hex
     std::unordered_set<int> horizontal_boundary;//contains left & right column
 
+    void get_x_y(int i, unsigned short &x, unsigned short &y) const {
+        x=i/board_ref.board_size;
+        y=i%board_ref.board_size;
+    }
+
+    void update_simulation_board(int x, char val){
+        sb[x]=val;
+    }
+
     unsigned short get_base_index(unsigned short x, unsigned short y) const{
-        return x*board.board_size + y;
+        return x*board_ref.board_size + y;
     }
 
     unsigned short get_base_index(Hex* h) const {
         unsigned short x,y;
         h->get_pos(x,y);
         return get_base_index(x,y);
+    }
+
+    double montecarlo_simulate(int pos, int count);
+
+    bool ai_won(std::vector<char> board);
+    
+    void print_sim_board() {
+        std::cout << "DEBUG: sim board = ";
+        for(auto i: sb)
+            std::cout << i << " ";
+        std::cout << std::endl;
     }
 };
 
